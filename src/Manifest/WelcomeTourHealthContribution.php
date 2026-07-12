a80d76d3c49a64e0514c98785621003ca4d57b83
Ben <howdu@users.noreply.github.com>
2026-07-12 07:44:22 +0100

    fix: align packages with 0.0 release line

--- src/Manifest/WelcomeTourHealthContribution.php
+++ src/Manifest/WelcomeTourHealthContribution.php
@@ -10,6 +10,6 @@ final class WelcomeTourHealthContribution implements ChecksExtensionHealth
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 }
