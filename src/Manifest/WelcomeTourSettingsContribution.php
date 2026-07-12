85994563c3ce07bea08b444b9e90b6b0735c2938
Ben <howdu@users.noreply.github.com>
2026-07-12 07:28:21 +0100

    fix: align package compatibility with 0.0 release line

--- src/Manifest/WelcomeTourSettingsContribution.php
+++ src/Manifest/WelcomeTourSettingsContribution.php
@@ -11,6 +11,6 @@ final class WelcomeTourSettingsContribution implements ExtensionContribution, Re
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 }
